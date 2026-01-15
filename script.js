a90ac3ecab9cf6e4fa475556d91ed290068cb512
Rohit Darunkar <139346362+rohitdarunkar@users.noreply.github.com>
2026-01-16 04:17:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function stringChop(str, size) {
-  // your code here
+  if (str === null) return [];
+
+  size = Number(size); 
+  const result = [];
+
+  for (let i = 0; i < str.length; i += size) {
+    result.push(str.substring(i, i + size));
+  }
+
+  return result;
 }
+
 
 // Do not change the code below
 const str = prompt("Enter String.");
